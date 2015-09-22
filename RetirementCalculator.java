ba9e25553fee5824e3a23c1635311bb610fc2746
Ingo Hotischeck <i.hotischeck@iks-gmbh.com>
2015-09-22 13:49:08 +0200

    Programm zur Berechnung der Zeit bis zum Rentenalter. Falscheingaben werden abgefangen.

---
+++ RetirementCalculator.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class RetirementCalculator {
+	public static void main (String args[]){
+		RetirementCalculator retireCalc = new RetirementCalculator();
+		retireCalc.calculation();
+		
+	}
+	
+	private void calculation() {
+		InputStream inputStream = System.in;
+		BufferedReader buffRead = new BufferedReader(new InputStreamReader(inputStream));
+		int exceptionChecker = 1;
+		Calendar calendar = Calendar.getInstance();
+		
+		do {
+			try {
+				int currentAge = getUserInput("What is your current age? ", buffRead);
+				int retireAge = getUserInput("At what age would you like to retire? ", buffRead);
+				int timeToRetire = retireAge - currentAge;
+				
+				if(currentAge<0 || retireAge <=0)
+				{
+					System.out.println("You can't be younger than 0. Please retype you age.\n");
+					continue;
+				}
+				
+				if (timeToRetire > 0) {
+					System.out.println("You have "+timeToRetire+" years left until you can retire.");
+					System.out.println("It's "+ calendar.get(Calendar.YEAR) + ", so you can retire in "+ (calendar.get(Calendar.YEAR)+timeToRetire) + ".");
+				}
+				else if (timeToRetire==0) {
+					System.out.println("Congratulations you did it! Go get retired!");
+				}
+				else {
+					System.out.println("You should have been retired by now!");
+				}
+				
+				exceptionChecker = 0;
+			} catch (Exception e) {
+				System.out.println("Sie haben eine Falscheingabe getätigt! Bitte wiederholen Sie die Eingabe.");
+			}
+		} while(exceptionChecker == 1);
+	}
+	
+	private int getUserInput(String message, BufferedReader buffRead) throws Exception{
+		System.out.print(message);
+		return Integer.parseInt(buffRead.readLine());
+	}
+}
